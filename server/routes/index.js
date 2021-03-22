90dccd39fa02e269cbf199c5bd59d773c04fc890
Bryce Rothhammer <rothhammer.b@gmail.com>
2021-03-21 20:37:37 -0500

    Add axios and create post route to twitter search api

---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const TWITTER_API_TOKEN = process.env.TWITTER_API_TOKEN
+const axios = require('axios');
+
+console.log(TWITTER_API_TOKEN);
+
+router.post('/search', async function (req, res) {
+  const { query } = req.body
+  const url = `https://api.twitter.com/1.1/search/tweets.json?${query}`
+
+  const result = await axios.get(url, { headers: { Authorization: `Bearer ${TWITTER_API_TOKEN}` } })
+    .then((response) => { return response.data })
+
+  return res.status(200).send(result)
+})
+
+module.exports = router
